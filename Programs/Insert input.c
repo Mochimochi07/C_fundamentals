daf2017330f890766fb5453e4398428f1b6d459e
Angel Jesse Dampil <108660194+Mochimochi07@users.noreply.github.com>
2023-06-06 09:22:47 +0800

    Create Insert input.c

---
+++ Programs/Insert input.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+char fullname;
+
+
+int main()
+  {
+  printf("Hello what is your name?);
+  scanf("%s" ,fullname);
+         
+  printf("Hello, %s ", fullname);
+  
+  
+  
+  return 0;
+  
+  }
